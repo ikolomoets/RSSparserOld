571c6dd08204103122dcb587f3719b7e7eafa2e7
EvanBS <evandiesp@gmail.com>
2018-11-07 23:40:04 +0200

    RSS XML parser

--- RSSproject/Controllers/HomeController.cs
+++ RSSproject/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RSSproject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,11 @@ namespace RSSproject.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.Users.ToList());
         }
 
         public ActionResult About()
