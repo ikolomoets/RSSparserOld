ff0e2da34f2fff2f6dd7bad9f53ac0bdc0910607
EvanBS <evandiesp@gmail.com>
2018-11-07 20:49:12 +0200

    Добавьте файлы проекта.

---
+++ RSSproject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RSSproject.MvcApplication" Language="C#" %>
---
+++ RSSproject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RSSproject.Startup))]
+namespace RSSproject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
